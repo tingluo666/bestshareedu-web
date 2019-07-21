a3a240b87ea6dd7cb37884b16ffcd80ccf9e93ac
tingluo666 <wanbowanbo@126.com>
2019-07-22 01:34:34 +0800

    修改测试环境url配置

--- config/conf.js
+++ config/conf.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV == 'development') { //开发环境
 }else if (process.env.NODE_ENV == 'testing') {  //测试环境
   module.exports =  {
     title: '优享教育',
-    baseUrl: 'http://106.15.39.80:5840/#/'  //java项目接口地址
+    baseUrl: 'http://106.15.39.80:5840/'  //java项目接口地址
   }
 }else {
   module.exports =  {   //默认生产环境
